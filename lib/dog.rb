3f37c7dd4742434d208e087a69cea1ae29b4ffff
Samantha brake <sammy.brake@gmail.com>
2018-09-16 14:52:43 +0000

    automatically backed up by learn

---
+++ lib/dog.rb
@@ -0,0 +1,21 @@
+class Dog 
+  
+  @@all = []
+  
+  attr_accessor :name 
+  
+  def initialize(name)
+    @name = name 
+    @@all << self 
+  end 
+  
+  def clear_all 
+    @@all = []
+  end 
+  
+  def all 
+    @@all.each do |dog|
+      puts dog 
+    end 
+  end 
+end 
